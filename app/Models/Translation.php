8f141095e9db90027c4d08b3897a8fab0ecebc2b
bytomo <bybyu.pc@gmail.com>
2025-08-15 00:28:01 +0700

    Create Translation.php

---
+++ app/Models/Translation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Translation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id','source_lang','target_lang','source_text','translated_text',
+        'provider','char_count','cost_estimate','meta'
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
